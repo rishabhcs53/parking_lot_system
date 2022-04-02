ee86002d231ee0cbf69997f0bf2f5ef5b5449150
rishabhcs53 <iitpacin@gmail.com>
2022-04-02 18:47:06 -0400

    commit all code

---
+++ src/main.py
@@ -0,0 +1,27 @@
+
+'''
+for time in simulation time
+
+    zones =4
+
+    occupancy = [0, 0, 0, 0]
+
+    time_to_take = [0, 2, 4 ,6]
+
+    random number geenrattor (>0.5) parking
+
+    i = findMin(occupany[i]+time[i])
+
+    occupancy[i] = occupancy[i]+1
+
+    random number geenrattor (<0.5) parking select zone (1-4)
+    if(occupancy[i]>0):
+        occupancy[i] = occupancy[i] - 1
+
+    closed[4]
+    open[4]
+'''
+
+if __name__=='__main__':
+    print('Welcome to Parking System')
+
